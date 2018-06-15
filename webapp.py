661dd2a2322d5e5b17052611c782494faf772ff3
arucard21 <arucard21@gmail.com>
2018-06-15 16:25:51 +0200

    Changed so this only uses a single API call

--- webapp.py
+++ webapp.py
@@ -8,20 +8,10 @@ api = Api(app)
 def mainSearchPage():
     return render_template('index.html')
 
-class Features(Resource):
-    def get(self):
-        return {}
-
-class Keywords(Resource):
-    def get(self):
-        return {}
-
 class Videos(Resource):
     def get(self):
         return {}
 
-api.add_resource(Features, '/api/features/')
-api.add_resource(Keywords, '/api/keywords/')
 api.add_resource(Videos, '/api/videos/')
 
 if __name__ == '__main__':
