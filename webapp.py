37b5311d15ae1c92c11a8dbfd886f0a9219cc14f
arucard21 <arucard21@gmail.com>
2018-06-15 16:36:49 +0200

    Make the API access the video URL that's provided (just returns it for now)

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -10,7 +10,9 @@ def mainSearchPage():
 
 class Videos(Resource):
     def get(self):
-        return {}
+		videoURL = request.args.get('videoURL')
+		# TODO download the video from the URL, process it and return the list of similar videos
+		return {'url': videoURL}
 
 api.add_resource(Videos, '/api/videos/')
 
