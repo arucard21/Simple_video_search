7d8dc9a62e66305d183b02742340e0452f1c4984
arucard21 <arucard21@gmail.com>
2018-06-26 07:00:34 +0200

    Put modified feature extractor code alongside the rest of the modified youtube8m code

--- webapp.py
+++ webapp.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from flask import Flask, render_template, request
 from flask_restful import Resource, Api
 import tensorflow as tf
-from extract_tfrecords_main import extract_features
+from youtube8m.feature_extractor.extract_tfrecords_main import extract_features
 from similarity import load_forest, similar_videos, similar_videos_from_forest
 from youtube8m.inference import infer
 from google.protobuf.json_format import MessageToJson
