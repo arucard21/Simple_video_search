13c904316def795043a8a1da8fc74e514cc1fa89
arucard21 <arucard21@gmail.com>
2018-06-28 08:39:00 +0200

    Show label names in the web interface instead of label IDs

---
+++ label_names.py
@@ -0,0 +1,17 @@
+import csv
+from datetime import datetime
+
+label_names = {}
+
+def get_label_name(label_id):
+	return label_names[label_id]
+
+def load_label_names():
+	global labels
+	
+	with open('vocabulary.csv') as csvfile:
+		reader = csv.reader(csvfile)
+		next(reader, None)
+		for rows in reader:
+			label_names[rows[0]] = rows[3]
+	print "[SimpleVideoSearch][{}] Done loading label names from CSV".format(datetime.now())
